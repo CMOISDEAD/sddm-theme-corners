c8123b8de082ae4a50885b489881929367f79112
camilo <camiloesteban661@gmail.com>
2024-08-26 20:19:51 -0500

    ref: refactor install.sh and remove doc/images

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-echo "Installing sddm-theme-corners"
+REPO_URL="https://github.com/cmoisdead/sddm-theme-corners.git"
+REPO_NAME="sddm-theme-corners"
+THEME_DIR="/usr/share/sddm/themes/corners"
 
-git clone https://github.com/cmoisdead/sddm-theme-corners.git
+echo "Installing sddm-theme-corners..."
 
-cd sddm-theme-corners/
+if [ -d "$REPO_NAME" ]; then
+  echo "Repository already exists. Please remove the directory and try again."
+  exit 1
+fi
 
-echo "Copying files to /usr/share/sddm/themes/"
+echo "Cloning repository from $REPO_URL..."
+git clone "$REPO_URL" || exit 1
 
-sudo cp -r corners/ /usr/share/sddm/themes/
+echo "Copying files to $THEME_DIR..."
+sudo cp -r "$REPO_NAME/corners/" "$THEME_DIR" || exit 1
 
-echo "Done."
+echo "Installation completed successfully."
